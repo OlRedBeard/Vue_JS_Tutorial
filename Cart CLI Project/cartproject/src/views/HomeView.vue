8b90543185d4b1b815d49d018174f3bf327a9543
OlRedBeard <gerrit.j.kat@gmail.com>
2022-06-09 21:22:32 -0600

    finished dynamic add to cart and products page, completed tutorial

--- Cart CLI Project/cartproject/src/views/HomeView.vue
+++ Cart CLI Project/cartproject/src/views/HomeView.vue
@@ -11,45 +11,13 @@
 
     <div class="recommended">
 
-      <div v-for="(product, i) in inventory.slice(0,3)" :key="product.id" class="card">
-        <div class="card-title">
-          {{ product.name }}
-        </div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-{{ product.icon }}"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>{{ product.type }}</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">
-                ${{ product.price.USD }}
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input type="number" v-model.number="product.quantity">
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button @click="addToCart(product.name, i)" class="btn btn-light">
-            Add to cart
-          </button>
-        </div>
-      </div>
+      <Card-Widget v-for="(product, index) in inventory.slice(0,3)"
+        :key="product.id"
+        class="card"
+        :index="index"
+        :product="product"
+        :addToCart="addToCart"
+      />
 
     </div>
 
@@ -57,9 +25,13 @@
 </template>
 
 <script>
+import CardWidget from '@/components/CardWidget.vue'
 
 export default {
   name: 'HomeView',
-  props: ['inventory']
+  props: ['inventory', 'addToCart'],
+  components: {
+    CardWidget
+  }
 }
 </script>
